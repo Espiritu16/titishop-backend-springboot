8b93205c5ea737c7c9cd2974e45a31231c4875fc
Espiritu16 <u23214543@utp.edu.pe>
2026-05-30 16:20:12 -0500

    chore: inicializa backend Spring Boot de TitiShop

---
+++ src/main/java/com/titishop/TitishopBackendApplication.java
@@ -0,0 +1,12 @@
+package com.titishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitishopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TitishopBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/com/titishop/TitishopBackendApplicationTests.java
@@ -0,0 +1,12 @@
+package com.titishop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TitishopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
