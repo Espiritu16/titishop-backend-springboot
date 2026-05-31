117cbde830d01e6bef70f9f73098baabf686766f
Felix <salimv2305@gmail.com>
2026-05-31 00:08:05 -0500

    feat(usuarios): exponer endpoints CRUD REST

--- src/main/java/com/titishop/usuarios/controller/UsuarioController.java
+++ src/main/java/com/titishop/usuarios/controller/UsuarioController.java
@@ -1,7 +1,21 @@
 package com.titishop.usuarios.controller;
 
+import com.titishop.usuarios.dto.ActualizarUsuarioRequest;
+import com.titishop.usuarios.dto.CrearUsuarioRequest;
+import com.titishop.usuarios.dto.UsuarioResponse;
 import com.titishop.usuarios.service.UsuarioService;
+import jakarta.validation.Valid;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +26,32 @@ public class UsuarioController {
 
 	public UsuarioController(UsuarioService usuarioService) {
 		this.usuarioService = usuarioService;
+	}
+
+	@GetMapping
+	public List<UsuarioResponse> listar() {
+		return usuarioService.listar();
+	}
+
+	@GetMapping("/{id}")
+	public UsuarioResponse obtenerPorId(@PathVariable UUID id) {
+		return usuarioService.obtenerPorId(id);
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public UsuarioResponse crear(@Valid @RequestBody CrearUsuarioRequest request) {
+		return usuarioService.crear(request);
+	}
+
+	@PutMapping("/{id}")
+	public UsuarioResponse actualizar(@PathVariable UUID id, @Valid @RequestBody ActualizarUsuarioRequest request) {
+		return usuarioService.actualizar(id, request);
+	}
+
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void inactivar(@PathVariable UUID id) {
+		usuarioService.inactivar(id);
 	}
 }
